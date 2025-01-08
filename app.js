a7133040d8c20a63ebdd4a1f6200344c56b29afc
Joshua Ike <iluhjosh13@gmail.com>
2025-01-08 20:51:31 +0100

    Done.

--- app.js
+++ app.js
@@ -3,93 +3,129 @@ import { Command } from "commander";
 import { request } from "https";
 const program = new Command();
 
-const errorHandler = message =>{
+const errorHandler = (message, exit = true) => {
 	console.error(chalk.bold.red(message));
-	process.exit(1);
-}
+	if (exit) {
+		process.exit(1);
+	}
+};
 
-const checkUsername = (value, previous) =>{
+const checkUsername = (value, previous) => {
 	const userNameRegex = /^[a-zA-Z0-9](?:-?[a-zA-Z0-9]){0,38}$/;
 	value = value.trim();
 	const test = userNameRegex.test(value);
-	if(!test){errorHandler("Invalid username")};
+	if (!test) {
+		errorHandler("Invalid username");
+	}
 	return value;
-}
+};
 
-const accumulate = () =>{
-	return 
-}
-
-const handleData = jsonData =>{
-	jsonData.forEach((data, index) =>{
-		if(data.type === "PushEvent"){
-			let number = 1;
-			// for(let _i = index; jsonData[_i].repo.name == data.repo.name && jsonData[_i].type == data.type; _i++){
-			// 	console.log(_i, number)
-			// 	number++;
-			// }
-			console.log(`- Pushed ${number} commits to ${data.repo.name}`)
-		} else if(data.type === "PublicEvent"){
-			const repoName = data.repo.name.split("/")[1];
-			console.log(`- Made thier repository, "${repoName}", public`)
-		} else if(data.type === "WatchEvent"){
-			console.log(`- Started watching the repository ${data.repo.name}`)
-		} else if(data.type === "CreateEvent" && !data.payload.ref){
-			const repoName = data.repo.name.split("/")[1];
-			console.log(`- Created the repository "${repoName}"`)
-		} else if(data.type === "IssueCommentEvent"){
-			console.log(`- Commented on an issue in ${data.repo.name}`)
-		} else if(data.type === "IssuesEvent" && data.payload.action === "created"){
-			console.log(`- Opened an issue in ${data.repo.name}`)
-		} else if(data.type === "IssuesEvent" && data.payload.action){
-			console.log(`- Issue created in ${data.repo.name} was ${data.payload.action}`)
-		} else if(data.type === "PullRequestEvent"){
-			console.log(`- Opened a pull request in ${data.repo.name}`)
+const handleData = (jsonData) => {
+	let prevActivity = {};
+	let messageArray = [];
+	const getMessage = (data) => {
+		let repoName = data.repo.name;
+		switch (data.type) {
+			case "PushEvent":
+				const { prevType, prevID, commitCount = 0 } = prevActivity;
+				if (data.type === prevType && data.repo.id === prevID) {
+					prevActivity.commitCount = commitCount + 1;
+					messageArray.pop();
+					return `- Pushed ${commitCount + 1} commits to ${repoName}`;
+				} else {
+					prevActivity = {
+						prevType: data.type,
+						prevID: data.repo.id,
+						commitCount: 1,
+					};
+					return `- Pushed 1 commit to ${repoName}`;
+				}
+			case "PublicEvent":
+				repoName = repoName.split("/")[1];
+				return `- Made thier repository, "${repoName}", public`;
+			case "WatchEvent":
+				return `- Started watching the repository ${repoName}`;
+			case "CreateEvent":
+				if (!data.payload.ref) {
+					repoName = repoName.split("/")[1];
+					return `- Created the repository "${repoName}"`;
+				}
+				break;
+			case "IssueCommentEvent":
+				return `- Commented on an issue in ${repoName}`;
+			case "IssuesEvent":
+				if (data.payload.action === "created") {
+					return `- Created an issue in ${repoName}`;
+				} else {
+					return `- Issue in ${repoName} was ${data.payload.action}`;
+				}
+			case "PullRequestEvent":
+				return `- Opened a pull request in ${repoName}`;
+			case "ForkEvemt":
+				return `- Forked the repository ${repoName}`;
+			default:
+				return undefined;
 		}
-	})
-}
+	};
 
-const fetchUser = username =>{
+	jsonData.forEach((data) => {
+		const message = getMessage(data);
+		if (message) {
+			messageArray.push(message);
+		}
+	});
+	
+	const filteredArray = messageArray.filter(
+		(data, index) => data !== messageArray[index + 1],
+	);
+	console.log(filteredArray);
+};
+
+const fetchUser = (username) => {
 	const options = {
 		hostname: "api.github.com",
 		path: `/users/${username}/events`,
 		port: 443,
 		method: "GET",
-		headers:{
-			"User-Agent": "node-app"
-		}
-	}
+		headers: {
+			"User-Agent": "node-app",
+		},
+	};
 
-	request(options, res =>{
-		if(res.statusCode == 404){
+	request(options, (res) => {
+		if (res.statusCode == 404) {
 			errorHandler(`User with the username ${username} not found`);
 		}
 
 		let data = "";
 
-		res.on("data", chunk => {
-			data+=chunk
+		res.on("data", (chunk) => {
+			data += chunk;
 		});
 
-		res.on("end", () =>{
-			try{
+		res.on("end", () => {
+			try {
 				const jsonData = JSON.parse(data);
-				handleData(jsonData)
-			}catch(err){
-				console.error("Error occured while parsing JSON: ", err.message)
+				handleData(jsonData);
+			} catch (err) {
+				errorHandler(
+					`Error occured while parsing JSON: ${err.message}`,
+					false,
+				);
 			}
+		});
+	})
+		.on("error", (err) => {
+			errorHandler(`Failed to fetch user details: ${err}`, false);
 		})
-	}).on("error", err =>{
-		console.error(`Failed to fetch user details: ${err}`)
-	}).end();
-}
-
+		.end();
+};
 
 program
 	.command("github-activity")
 	.argument("<username>", "Github username", checkUsername)
-	.action(username =>{
-		fetchUser(username)
-	})
+	.action((username) => {
+		fetchUser(username);
+	});
 
 program.parse();
